20561e2d9ac97f747d6fe7f05e4016447e87d9ab
a1810246 <71242560+a1810246@users.noreply.github.com>
2023-09-04 13:09:33 +0000

    test

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    vector<int> array = {1, 3, 5, 4, -5, 100, 7777, 2014};
+    /*vector<int> array = {1, 3, 5, 4, -5, 100, 7777, 2014};
     cout << "Given array is \n";
     for (int& num : array)
     {
@@ -16,5 +16,23 @@ int main()
     cout<<endl;
 
     return 0;
+    */
+   vector<int> data = {9, 7, 5, 11, 12, 2, 14, 3, 10, 6};
+    QuickSort quickSort;
 
+    cout << "Original Data: ";
+    for (int num : data) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    vector<int> sortedData = quickSort.sort(data);
+
+    cout << "Sorted Data: ";
+    for (int num : sortedData) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
 }

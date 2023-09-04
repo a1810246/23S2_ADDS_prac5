609497c3f49cc75c6a2b6dd6c9810bd8a9b3e355
a1810246 <71242560+a1810246@users.noreply.github.com>
2023-09-04 12:34:35 +0000

    test

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 
 vector<int> BubbleSort::sort(vector<int> list){
-    for(int i=0; i<list.size()-1; i++){
-        for(int j=0; j<list.size()-1-i; i++){
+    int size = list.size();
+    for(int i=0; i<size-1; i++){
+        for(int j=0; j<size-1-i; i++){
             if(list[j]>list[j+1]){
                 swap(list[j],list[j+1]);
             }
         }
     }
+    return list;
 }
